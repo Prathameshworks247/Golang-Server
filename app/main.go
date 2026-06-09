2de9dc21c2241bd59a578861cd09352aa0e9a671
Prathameshworks247 <prathameshworks247@gmail.com>
2026-06-10 00:58:55 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -6,9 +6,6 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -22,9 +19,11 @@ func main() {
 		os.Exit(1)
 	}
 	
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+conn.Close()
 }
